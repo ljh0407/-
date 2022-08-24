5331515f9ee9fa4c5815a638a2bbb861c0420427
504 <504@DESKTOP-D4S2NMH>
2022-08-24 18:01:28 +0900

    8/24

---
+++ front/public/JS/객체/10.Dom객체5.js
@@ -0,0 +1,60 @@
+/**
+ * 
+ */
+ 
+ let counter = 0 // 클릭수를 저장하는 변수
+ let isconnect = false // 연결 상태 저장하는 변수
+ const h1 = document.querySelector('h1') // 요소 가져오기
+ const p = document.querySelector('p') // 요소 가져오기
+ const connectBtn = document.querySelector('#connect') // 요소 가져오기
+ const disconnectBtn = document.querySelector('#disconnect') // 요소 가져오기
+ const upbtn = document.querySelector('#upbtn')
+ const dawnbtn = document.querySelector('#dawnbtn')
+ //출력하는 함수
+ const listener = () => { h1.textContent = `출력횟수 : ${++counter}`}
+ 
+ // 1. 연결 버튼을 클릭했을때
+ connectBtn.addEventListener( 'click' , ()=> {
+	if( isconnect == false){ // 만약에 연결이 안되어 있으면
+		h1.addEventListener('click' , listener )// h1태그에 클릭 이벤트 주기
+		p.textContent = '이벤트 연결 상태 : 연결' // p 태그에 내용 넣기
+		isconnect = true // 연결 했다는 의미로 연결상태 변수에 true 로 변환
+		
+	}
+})
+ 
+upbtn.addEventListener( 'click' , () => {
+	if( isconnect == true ){
+		counter++
+		h1.textContent = `출력횟수 : ${counter}` 
+	}
+})	
+dawnbtn.addEventListener('click' , ()=> {
+	
+})
+ 
+ 	
+ 	
+// 2. 연결해제 버튼을 클릭했을때
+disconnectBtn.addEventListener( 'click', () => {
+	if(isconnect == true){ // 만약에 연결이 되어 있으면
+	   h1.removeEventListener('click' , listener) // h1태그에 클릭 이벤트 삭제
+	   p.textContent = '이벤트 연결 상태 : 해제'
+	   isconnect = false // 해제 했다는 의미로 연결상태 변수에 false 로 변환
+	}
+})
+
+
+ // 객체명.addEventListener('이벤트명' , 클릭함수 )
+ 	// 해당 객체의 이벤트 발생했을때 콜백함수 실행 된다~
+//// 1.
+// h1.addEventListener('click' , (event) => {
+//	counter++  // 변수에 1증가
+//	h1.textContent =`클릭횟수 : ${counter}`
+//}) // 이벤트 함수 end
+//	
+//// 2. html 에서 클릭이벤트 속성 이용한다.
+//function 함수(){
+//	counter ++ // 변수에 1증가
+//	h1.textContent =`클릭횟수 : ${counter}`
+//}
---
+++ front/public/JS/객체/9.Dom객체4.js
@@ -0,0 +1,72 @@
+/**
+ * 
+ */
+ 
+ // p.320
+ // 1. js <h1>태그 생성 -> 객체 대입
+ const header = document.createElement('h1')
+ // 2. 객체 텍스트 조작
+ header.textContent = 'js에서 만든 태그'
+ header.style.color = 'white'
+ header.style.backgroundColor='black'
+ // 3. 해당 태그에 하위태그로 객체를 추가
+ document.body.appendChild(header)
+ 
+ // p.321 
+ 	// 1. 해당 id의 태그 객체 가져오기
+ const divA = document.querySelector('#first')
+ const divB = document.querySelector('#second')
+ const h1 = document.createElement('h1') // 2. 태그(요소) 만들기
+ h1.textContent = '이동하는 h1 태그'  // 3. js만든 태그(요소)에 텍스트 넣기
+ 
+ const tofirst = () => { 
+	divA.appendChild(h1) // 4. 첫번째 div에 js만든 태그 추가 
+	setTimeout( tosecond  , 2000 ) // 5. 1초뒤에 다른 함수 [tosecond] 실행
+}
+const tosecond = () => {
+	divB.appendChild(h1)
+	setTimeout( tofirst , 2000 ) // 6. 2초뒤에 다른 함수 [tosecond] 실행
+}	
+
+tofirst() // 6. 함수 실행
+ /*
+ // 1. 기본함수
+ function 함수( ){
+	divB.appendChild(divA)
+}
+ // 2. 익명함수
+ const 익명함수 = function ( ){ divB.appendChild(divA) }
+ 
+ const tosecond = () => {
+	divB.appendChild(divA)
+}
+
+// 3. 화살표 함수 
+const 화살표함수 = ( ) => {divB.appendChild(divA) }
+
+// 4. 콜백함수
+// 함수명( 함수 ) 
+// 3. 타이머함수( 콜백함수( ) , 밀리초(1000/1초) )
+setTimeout( tosecond , 3000 ) */
+
+// p. 322 객체 제거
+
+const h2 = document.querySelector('h2')
+// 1. 타이머함수 : setTimeout( 함수 , 밀리초 )
+// 2. 문서객체.removeChild( 자식 객체 )
+// 3. 부모찾기 : 문서객체.parentNode(문서객체)
+// 1.
+setTimeout( () => {
+h2.parentNode.removeChild(h2)
+} , 3000 ) // 3초후 실행되는 코드
+	
+// 2.
+const 익명함수 = () => { h2.parentNode.removeChild(h2) }	
+setTimeout( 익명함수 , 3000 )
+
+// 3. 
+function 함수(){
+//	h2.parentNode.removeChild(h2)
+	document.body.removeChild(h2)
+}
+setTimeout( 함수 , 3000 )
