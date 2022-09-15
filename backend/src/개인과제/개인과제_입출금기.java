70c3cf6f06d60fff8790e7df918aa17e548f6c82
504 <504@DESKTOP-D4S2NMH>
2022-09-15 17:49:30 +0900

    9/15

--- backend/src/개인과제/개인과제_입출금기.java
+++ backend/src/개인과제/개인과제_입출금기.java
@@ -9,7 +9,7 @@ public class 개인과제_입출금기 {
 		Scanner scanner = new Scanner(System.in);
 		
 		int 비밀번호 = 1234;
-		int 통장잔액 = 3000000;
+		
 		while(true) {
 		// 첫화면
 		System.out.println("현금 자동 입출금기");
@@ -20,44 +20,52 @@ public class 개인과제_입출금기 {
 		if( 버튼 == 1 ) {
 			System.out.println("입금할 금액을 넣어주세요 : "); int 입금액 = scanner.nextInt();
 			System.out.println(입금액+"원을 입금하시겠습니까?");
-			System.out.println("1.예 2.아니요"); int 버튼3 = scanner.nextInt();
+			System.out.println("1.예\t 2.아니요"); int 버튼3 = scanner.nextInt();
 			if( 버튼3 == 1 ) {}
 			else if(버튼3 == 2) {
 				System.out.println("거래를 종료합니다.");break;
 			}
 			
 			System.out.println("입금이 완료되었습니다. 거래를 더 하시겠습니까?");
-			System.out.println("1.거래 더하기 2.거래종료"); int 버튼2 = scanner.nextInt();
+			System.out.println("1.거래 더 하기 2.거래종료"); int 버튼2 = scanner.nextInt();
 			if( 버튼2 == 1 ) {// 거래 더 하기  초기화면으로 이동
 				
 			}
 			else if ( 버튼2 == 2 ) { // 거래종료 인삿말
-				System.out.println("거래 감사합니다. 카드를 받아주세요.");
-			}break;
+				System.out.println("거래 감사합니다. 카드를 받아주세요.");break;
+			}
 			
 			
 		}
 		else if ( 버튼 == 2 ) {
-			System.out.println("비밀번호를 입력해주세요."); int 입력번호 = scanner.nextInt();
-			if( 비밀번호 != 입력번호 ) {System.err.println("비밀번호가 틀렸습니다."); break;}
-		
-			System.out.println("출금할 금액을 입력해주세요."); int 출금액 = scanner.nextInt();
-			System.out.println(출금액 + "원을 인출하시겠습니까?");
-			System.out.println("1.예\t2.아니요"); int 출금btn = scanner.nextInt();
-			if( 출금btn == 1) { 
-				System.out.println(출금액+"원이 인출되었습니다.\n감사합니다."); break;
-			}else if ( 출금btn == 2) {
-				System.out.println("거래를 종료합니다."); break;
-			}else {}
+			for(int i = 0; i<5; i++) {
+				System.out.println("비밀번호를 입력해주세요."); int 입력번호 = scanner.nextInt();
+				if( 비밀번호 == 입력번호 ) {
+					
+					
+					System.out.println("출금할 금액을 입력해주세요."); int 출금액 = scanner.nextInt();
+					System.out.println(출금액 + "원을 인출하시겠습니까?");
+					System.out.println("1.예\t2.아니요"); int 출금btn = scanner.nextInt();
+					if( 출금btn == 1) { 
+						System.out.println(출금액+"원이 인출되었습니다.\n감사합니다."); break;
+					}else if ( 출금btn == 2) {
+						System.out.println("거래를 종료합니다."); break;
+					}else {}
+					
+					break;
+				}else {
+					System.err.println("비밀번호가 틀렸습니다."); 
+					
+				}
+					
+			}
+			
 			
 			
 		}
-//		else if ( 버튼 == 3 ) {System.out.println("잔액 조회를 하시겠습니까?"); int  = scanner.nextInt();
-	//	}
-		else if ( 버튼 == 3 ) {System.out.println("감사합니다. 카드를 받아주세요.");break;}
+
+		else if ( 버튼 == 3 ) {System.out.println("감사합니다. \n카드를 받아주세요.");break;}
 		else {}
-			
-			
 			
 			
 		
