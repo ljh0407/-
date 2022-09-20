4b6c900a2bfcaad7643d5865b3fef3abcd273d37
504 <504@DESKTOP-D4S2NMH>
2022-09-20 20:04:46 +0900

    9/20

---
+++ backend/src/Level/개인과제/Level1.java
@@ -0,0 +1,19 @@
+package Level.개인과제;
+
+public class Level1 {
+
+	public static void main(String[] args) {
+		
+		System.out.println(" >> Level1_1 answer >> \n");
+		
+		boolean 불 = true; 		System.out.println("boolean : " +불);
+		byte 바이트 = 100; 		System.out.println(바이트);
+		char 차 = 'A';			System.out.println(차);
+		short 쇼트 = 30000; 		System.out.println(쇼트);
+		int 인트 = 2000000000; 	System.out.println(인트);
+		long 롱 = 4000000000L;  	System.out.println(롱);
+		
+		
+	}
+	
+}
---
+++ backend/src/Level/개인과제/Level2.java
@@ -0,0 +1,16 @@
+package Level.개인과제;
+
+public class Level2 {
+
+	public static void main(String[] args) {
+		
+		System.out.println(" >> Level1_2 answer >> ");
+		System.out.println(" >> answer time : " + LocalDateTime.now() +" >> \n" );
+		System.out.printf("%-10s   %15s   %30s\n" , "type" , "value" , "range" );
+		System.out.println("-------------------------------------------------------------");
+		
+		
+		
+	}
+	
+}
