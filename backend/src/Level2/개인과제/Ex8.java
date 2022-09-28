7d94ca57baea28e0f9357066d29278e61d5ca266
504 <504@DESKTOP-D4S2NMH>
2022-09-28 21:31:49 +0900

    .

---
+++ backend/src/Level2/개인과제/Ex8.java
@@ -0,0 +1,30 @@
+package Level2.개인과제;
+
+import java.time.LocalDateTime;
+import java.util.Scanner;
+
+
+public class Ex8 {
+
+	public static void main(String[] args) {
+		System.out.println(" >> Level2_8 answer >> ");
+		System.out.println(" >> answer time : " + LocalDateTime.now() +" >> \n" );
+		Scanner scanner = new Scanner(System.in);
+	
+		/* 문제풀이 위치 */
+		
+		
+		
+		/* ----------- */
+
+	}
+}
+/* 
+	[문제] 그림과 같이 계속입력한 수에 따라 누적으로 저장되어 저장된 수 만큼 ■ 를 출력하시오.
+	[조건] 
+		1. 문제풀이 위치 외 코드는 수정 불가 합니다.
+		2. 만약에 누적값이 0 보다 작으면 종료됩니다.
+*/
+	
+	
+
