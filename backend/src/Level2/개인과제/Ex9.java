a5eeec5e88044d182512b62da1bedaa38718adad
pc <pc@14.35.8.205>
2022-10-06 23:05:21 +0900

    개인과제

--- backend/src/Level2/개인과제/Ex9.java
+++ backend/src/Level2/개인과제/Ex9.java
@@ -1,16 +1,34 @@
 package Level2.개인과제;
 
+import java.util.Scanner;
+
 public class Ex9 {
 
 	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.println("============간단 계좌만들기============");
+		System.out.println("1.계좌만들기\t2.계좌목록\t3.입금\t4.출금");
+		System.out.println("선택 : ");
+		int ch = scanner.nextInt();
+		if(ch == 1) {// 계좌만들기
+			String 이름 = scanner.next();
+			String 주민등록번호 = scanner.next();
+			String 전화번호 = scanner.next();
+			int 입금액 = scanner.nextInt();
+			System.out.println("이름 : "+이름);
+			System.out.println("주민등록번호 : "+주민등록번호);
+			System.out.println("전화번호 : "+전화번호);
+			System.out.println("초기 입금액 : "+입금액);
+		}else if(ch == 2) {}
+		else if(ch == 3) {}
+		else if(ch == 4) {}
 		
 		
-		
-	}
 	
+	}// main end
 	
 }
-
 /*
 	-*- 자유 주제 미니 프로젝트  -*-
 	[조건]
